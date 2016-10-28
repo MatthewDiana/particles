fd2b2c7e28cd5c20e2840d4aaa13b99f22dc3d29
Matthew Diana <mdiana2@binghamton.edu>
2016-10-27 22:46:58 -0400

    Fix indentation issues

--- js/main.js
+++ js/main.js
@@ -19,37 +19,36 @@ function preload() {
 }
 
 function create() {
-  game.stage.backgroundColor = "#FFFFFF";
-	gameText = game.add.text(50, 50, 'Dodge the particles. Good luck.');
+    game.stage.backgroundColor = "#FFFFFF";
+    gameText = game.add.text(50, 50, 'Dodge the particles. Good luck.');
 
 	music = game.add.audio('cerebral_infection');
 	music.volume = 0.20;
 	music.play();
 
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-  player = game.add.sprite(game.world.centerX - 16, game.world.centerY - 16, 'blueball');
-  game.physics.arcade.enable(player);
-	player.body.collideWorldBounds = true;
-  enemyBallGroup = game.add.physicsGroup();
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    player = game.add.sprite(game.world.centerX - 16, game.world.centerY - 16, 'blueball');
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    enemyBallGroup = game.add.physicsGroup();
 
-  ballCount = 0;
+    ballCount = 0;
 	respawnDelay = 3000;
-  gameOver = false;
-  maxBallSpeed = 300;
-
-  ballSpawnTimer = game.time.create(false);
+    gameOver = false;
+    maxBallSpeed = 300;
+    
+    ballSpawnTimer = game.time.create(false);
 	game.time.events.add(Phaser.Timer.SECOND * 12, startSpawning, this);
 }
 
 function update() {
     game.physics.arcade.moveToPointer(player, 600);
     if (!Phaser.Rectangle.contains(player.body, game.input.x, game.input.y)) {
-			player.body.x = game.input.x;
-			player.body.y = game.input.y;
-      player.body.velocity.setTo(0, 0);
+        player.body.x = game.input.x;
+        player.body.y = game.input.y;
+        player.body.velocity.setTo(0, 0);
     }
-
-		game.physics.arcade.collide(enemyBallGroup, enemyBallGroup);
+    game.physics.arcade.collide(enemyBallGroup, enemyBallGroup);
     game.physics.arcade.collide(player, enemyBallGroup, collisionHandler, processHandler, this);
 }
 
@@ -61,33 +60,33 @@ function startSpawning() {
 }
 
 function spawnBall() {
-  if (!gameOver) {
-    let newBall = game.add.sprite( game.world.randomX, game.world.randomY, 'redball');
-    ballCount++;
-    newBall.alpha = 0;
-    let fadeIn = game.add.tween(newBall).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
-    fadeIn.onComplete.add(function() {
-      game.physics.arcade.enable(newBall);
+    if (!gameOver) {
+        let newBall = game.add.sprite( game.world.randomX, game.world.randomY, 'redball');
+        ballCount++;
+        newBall.alpha = 0;
+        let fadeIn = game.add.tween(newBall).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+        fadeIn.onComplete.add(function() {
+            game.physics.arcade.enable(newBall);
 			let randXVelocity = Math.floor(Math.random() * (maxBallSpeed + maxBallSpeed) + 1) - maxBallSpeed;
 			let randYVelocity = Math.floor(Math.random() * (maxBallSpeed + maxBallSpeed) + 1) - maxBallSpeed;
 			console.log(randXVelocity, randYVelocity);
-      newBall.body.velocity.setTo(randXVelocity, randYVelocity);
-      newBall.body.collideWorldBounds = true;
-      newBall.body.bounce.set(1);
+            newBall.body.velocity.setTo(randXVelocity, randYVelocity);
+            newBall.body.collideWorldBounds = true;
+            newBall.body.bounce.set(1);
 			enemyBallGroup.add(newBall);
-    }, this);
-  }
+        }, this);
+    }
 }
 
 function processHandler(player, enemyBall) {
-  return true;
+    return true;
 }
 
 function collisionHandler(player, enemyBall) {
-  if (!gameOver) {
-		gameOver = true;
-		music.stop();
-    game.physics.arcade.gravity.y = 1800;
-    gameText = game.add.text(50, 50, "Final score: " + ballCount + " balls")
-  }
+    if (!gameOver) {
+        gameOver = true;
+        music.stop();
+        game.physics.arcade.gravity.y = 1800;
+        gameText = game.add.text(50, 50, "Final score: " + ballCount + " balls")
+    }
 }
